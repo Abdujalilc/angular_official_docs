20cd9a8dd23af475c23ed8124cec788b4ffdc022
Abdujalilc <abdujalilc@gmail.com>
2022-04-07 17:21:01 +0500

    9 files modified, 2 added, 2 deleted

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,12 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
