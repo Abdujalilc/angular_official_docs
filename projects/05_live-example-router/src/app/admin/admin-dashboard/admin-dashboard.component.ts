2cc8e6180bfe551a8a5207848893d99afb44ed33
Abdujalilc <abdujalilc@gmail.com>
2022-06-08 17:59:54 +0500

    angular official docs cleaned

--- projects/05_live-example-router/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ projects/05_live-example-router/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -8,7 +8,6 @@ import { SelectivePreloadingStrategyService } from '../../selective-preloading-s
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
   sessionId!: Observable<string>;
