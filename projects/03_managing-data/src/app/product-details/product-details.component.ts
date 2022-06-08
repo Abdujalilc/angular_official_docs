f08ae4f57fee492c10fb3c5eded5845ba845bddd
Abdujalilc <abdujalilc@gmail.com>
2022-06-08 16:38:44 +0500

    03-lesson

--- projects/03_managing-data/src/app/product-details/product-details.component.ts
+++ projects/03_managing-data/src/app/product-details/product-details.component.ts
@@ -6,7 +6,6 @@ import { Product, products } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
